f28c95100ee1574333e94a8aab1df825859b65f0
Derick Germano <derick_gdo@hotmail.com>
2023-09-23 03:14:21 -0300

    create carousel
    foi criado o componente carousel para utilização nas pages login e cadastro

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,43 +1,58 @@
+import Carousel from "@/components/Carousel";
+
 export default function Login() {
 
   return (
     <>
       <div className="h-screen flex">
-        <div className="w-1/3 bg-gradient-to-b from-green-800 to-green-300 hidden sm:block">    
-          <h1 className="mt-4 m-9 text-white  text-4xl">Organiza</h1>      
+        <div className="hidden lg:flex items-center justify-center w-1/3 bg-gradient-to-b from-green-800 to-green-300 ">
+          <div className="w-screen">    
+            <h1 className="fixed top-4 left-9 text-white text-4xl">Organiza</h1>
+            <div className="p-5" >
+            <Carousel />
+            </div>            
+          </div>                
         </div>
-        <div className="block sm:flex w-2/3 justify-center items-center bg-white">
-          <div className="flex flex-wrap content-center justify-center rounded-l-md bg-white" style={{ width: '43rem', height: '32rem' }}>
-            <div className="w-80">
-                <h1 className="text-3xl font-semibold">Bem-vindo de volta!</h1>
-                <small className="text-gray-400">Por favor insira seus dados</small>
-                
-                <form className="mt-5">
-                  <div className="mb-6">
-                    <label className="mb-2 block text-sm font-semibold">Email</label>
-                    <input type="email" placeholder="Digite seu e-mail" className="block w-full rounded-md border border-gray-300 focus:border-green-700 focus:outline-none focus:ring-1 focus:ring-green-700 py-1 px-1.5 text-gray-500" />
-                  </div>
 
-                  <div className="mb-6">
-                    <label className="mb-2 block text-sm font-semibold">Senha</label>
-                    <input type="password" placeholder="*****" className="block w-full rounded-md border border-gray-300 focus:border-green-700 focus:outline-none focus:ring-1 focus:ring-green-700 py-1 px-1.5 text-gray-500" />
-                  </div>
-
-                  <div className="mb-3 flex justify-center">            
-                    <a href="#" className=" text-sm font-semibold text-green-700">Esqueceu sua senha?</a>
-                  </div>
-
-                  <div className="mb-3">
-                    <button className="mb-1.5 block w-full text-center text-white bg-green-700 hover:bg-green-900 px-2 py-1.5 rounded-md">Iniciar sessão</button>
-                  </div>
-                </form>
-
-                <div className="text-center">
-                  <span className="text-xs text-gray-400 font-semibold">Não tem uma conta?</span>
-                  <a href="#" className="text-xs border-b border-green-700  font-semibold text-green-700"> Criar conta</a>
-                </div>
-            </div>
+        <div className="lg:flex lg:w-2/3 w-screen justify-center items-center bg-white">
+          <div className="block lg:hidden bg-gradient-to-r from-green-800 to-green-300 p-4">
+            <h1 className="text-4xl text-white">Organiza</h1>
           </div>
+          <div className="p-7 h-auto bg-white">
+            <h1 className="text-3xl font-semibold">Bem-vindo de volta!</h1>
+            <small className="text-gray-400">Por favor insira seus dados</small>
+                
+            <form className="mt-5">
+              <div className="mb-6">
+                <label className="mb-2 block text-sm font-semibold">Email</label>
+                <input type="email" placeholder="Digite seu e-mail" className="block w-full rounded-md border border-gray-300 focus:border-green-700 focus:outline-none focus:ring-1 focus:ring-green-700 py-1 px-1.5 text-gray-500" />
+              </div>
+
+              <div className="mb-6">
+                <label className="mb-2 block text-sm font-semibold">Senha</label>
+                <input type="password" placeholder="*****" className="block w-full rounded-md border border-gray-300 focus:border-green-700 focus:outline-none focus:ring-1 focus:ring-green-700 py-1 px-1.5 text-gray-500" />
+              </div>
+
+              <div className="mb-3 flex justify-center">            
+                <a href="#" className=" text-sm font-semibold text-green-700">Esqueceu sua senha?</a>
+              </div>
+
+              <div className="mb-3">
+                <button className="mb-1.5 block w-full text-center text-white bg-green-700 hover:bg-green-900 px-2 py-1.5 rounded-md">Iniciar sessão</button>
+              </div>
+            </form>
+
+            <div className="text-center mb-6
+            ">
+              <span className="text-xs text-gray-400 font-semibold">Não tem uma conta?</span>
+              <a href="#" className="text-xs border-b border-green-700  font-semibold text-green-700"> Criar conta</a>
+            </div>
+
+            <div className="block lg:hidden">          
+            <Carousel />
+          </div>
+          </div>
+
         </div>
       </div>
     </>
