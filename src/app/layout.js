e9677da4be7eb4bb19497ab8d10ba36a8215adb8
Derick Germano <derick_gdo@hotmail.com>
2023-10-01 22:47:24 -0300

    delete import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
 import AuthProvider from '@/components/login-cadastro/AuthProvider'
-import Header from '@/components/login-cadastro/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
